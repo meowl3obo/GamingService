50273e6b71196bdc5dff6f60024802a642634e92
meowl3obo <dear91304526@gmail.com>
2023-04-29 17:41:41 +0800

    chore: run in 8080 port

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 	r := gin.Default()                   //1. 註冊一個路由器
 	r.RedirectFixedPath = true           //   自動修正url 允許大小寫
 	controller.NewController(r).Router() //2. 建立新的Router
-	r.Run("0.0.0.0:1234")                //3. 執行
+	r.Run("0.0.0.0:8080")                //3. 執行
 }
 
 func loadEnv() {
